acf05bbe3e76315907927bb34ea4970c0d56d978
renatorrocha <renatorrodrigues2002@gmail.com>
2025-05-01 12:54:32 -0300

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({
+	routeTree,
+	context: {},
+	defaultPreload: "intent",
+	scrollRestoration: true,
+	defaultStructuralSharing: true,
+	defaultPreloadStaleTime: 0,
+});
+
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
+
+const rootElement = document.getElementById("app");
+if (rootElement && !rootElement.innerHTML) {
+	const root = ReactDOM.createRoot(rootElement);
+	root.render(
+		<StrictMode>
+			<RouterProvider router={router} />
+		</StrictMode>,
+	);
+}
