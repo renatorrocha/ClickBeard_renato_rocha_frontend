f1e134d23abfded137facae01090e4500e6fa64c
renatorrocha <renatorrodrigues2002@gmail.com>
2025-05-07 16:00:34 -0300

    feat: integrate appointment fetching in admin dashboard
    - Replaced static appointment data with a query to fetch appointments using `useGetAppointments`.
    - Added loading state with a spinner to enhance user experience while data is being retrieved.

--- src/routes/_authenticated/admin/dashboard.tsx
+++ src/routes/_authenticated/admin/dashboard.tsx
@@ -6,8 +6,10 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
+import { useGetAppointments } from "@/lib/queries/appointments";
 import { createFileRoute } from "@tanstack/react-router";
 import { addDays, format } from "date-fns";
+import { Loader2 } from "lucide-react";
 
 export const Route = createFileRoute("/_authenticated/admin/dashboard")({
 	component: RouteComponent,
@@ -21,56 +23,21 @@ export type Appointment = {
 	canceledAt?: Date;
 };
 
-const appointments: Appointment[] = [
-	{
-		id: "1",
-		clientName: "Pedro Alves",
-		barberId: "1",
-		service: "Corte de Tesoura",
-		date: new Date(2025, 4, 2, 9, 0), // May 2, 2025, 9:00 AM
-		canceledAt: new Date(2025, 4, 2, 9, 30),
-	},
-	{
-		id: "2",
-		clientName: "Lucas Mendes",
-		barberId: "2",
-		service: "Sobrancelha",
-		date: new Date(2025, 4, 5, 10, 0), // May 2, 2025, 10:00 AM
-	},
-	{
-		id: "3",
-		clientName: "Marcos Pereira",
-		barberId: "1",
-		service: "Barba",
-		date: new Date(2025, 4, 5, 14, 30), // May 2, 2025, 2:30 PM
-		canceledAt: new Date(2025, 4, 2, 14, 30),
-	},
-	{
-		id: "4",
-		clientName: "Rafael Costa",
-		barberId: "3",
-		service: "Corte Degradê",
-		date: new Date(2025, 4, 5, 11, 0), // May 3, 2025, 11:00 AM
-	},
-	{
-		id: "5",
-		clientName: "Bruno Ferreira",
-		barberId: "2",
-		service: "Corte Degradê",
-		date: new Date(2025, 4, 5, 16, 0), // May 3, 2025, 4:00 PM
-	},
-	{
-		id: "6",
-		clientName: "Gustavo Lima",
-		barberId: "3",
-		service: "Sobrancelha",
-		date: new Date(2025, 4, 5, 9, 30), // May 4, 2025, 9:30 AM
-	},
-];
-
 function RouteComponent() {
+	const { data: appointments, isLoading } = useGetAppointments();
+
+	console.log(appointments);
+
+	if (isLoading) {
+		return (
+			<div className="flex justify-center items-center h-full">
+				<Loader2 className="h-4 w-4 animate-spin" />
+			</div>
+		);
+	}
+
 	const appointmentsByDate = appointments.reduce(
-		(acc, appointment) => {
+		(acc: Record<string, Appointment[]>, appointment: Appointment) => {
 			const dateKey = format(appointment.date, "yyyy-MM-dd");
 			if (!acc[dateKey]) {
 				acc[dateKey] = [];
